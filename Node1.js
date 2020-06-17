a8e83b2dc0b258e8dc31cf091df2e6d8a46b5c8a
manojg9 <mgurnani99@gmail.com>
2020-06-17 18:49:30 +0530

    Add files via upload
    modified script to remove hardcoding of file path and added exception handler.

--- Node1.js
+++ Node1.js
@@ -2,37 +2,27 @@ const axios = require('axios')
 const fs = require('fs')
 const handlebars = require('handlebars')
 const url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
-//var json_data;
+const path = 'C:\\manoj\\intern\\'
+const json_file = path + 'weatherdata.json'
+const templatefile = path + 'weatherdata_template.html'
+const outFile = path + 'weatherdata_report.html'
+
+
 axios.get(url).then(function(response,data) 
-//	console.log(data);
 {
-	//console.log(response.data)
-	fs.writeFile('C:\\manoj\\intern\\weatherdata.json',JSON.stringify(response.data),function(err) {})
-});
-  const json_data = fs.readFileSync('C:\\manoj\\intern\\weatherdata.json', 
-            {encoding:'utf8', flag:'r'}); 
-  var source = fs.readFileSync('C:\\manoj\\intern\\weatherdata_template.html', 
-          {encoding:'utf8', flag:'r'}); 
-      //var source1 = $("#weatherdata_template").html();
-      //var templateText = $("#tableTemplate").html();
-    var tableTemplate = handlebars.compile(source);
-    const result = tableTemplate(JSON.parse(json_data))
-  //  const html_result=result.html;
-    //console.log(html_result)
+ try {
+ 	  fs.writeFileSync(json_file,JSON.stringify(response.data),'utf8');
+      const json_data = fs.readFileSync(json_file,{encoding:'utf8', flag:'r'}); 
+      var source = fs.readFileSync(templatefile,{encoding:'utf8', flag:'r'}); 
+      var tableTemplate = handlebars.compile(source);
+      const result = tableTemplate(JSON.parse(json_data));
+      fs.writeFileSync(outFile,result);
+	
+   }
+    catch(err) { console.log(err) }
 
-    fs.writeFileSync('C:\\manoj\\intern\\weatherdata_report.html',result)
-
-    //var json_parsed = JSON.parse(json_data)
-    //console.log(json_parsed.weather)
-  //  console.log(tableTemplate)
-    //var html = templateScript(json_data);
-    //var html = tableTemplate(json_data);
-//    $('body').append(tableTemplate(json_data));
-    //console.log(html);
-    //$("#content").html(html);
-  //  console.log(json_data.name);
-    // Display the file content 
-    //json_data = data 
-
-//var weather_data = document.getElementByID('weatherdata').innerHTML;
+})
+   .catch(function (error) {
+    console.log('Error encountered while fetching data from '+ url + ' '+ error);
+  });
 
